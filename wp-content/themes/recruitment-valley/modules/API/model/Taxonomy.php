202ddc89daf7583cb4409434f1ee00dacbf5f94e
mimasdazai <anggit@madeindonesia.com>
2023-08-03 17:34:30 +0700

    add structure mdoel taxonomy and term

---
+++ wp-content/themes/recruitment-valley/modules/API/model/Taxonomy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Model;
+
+use WP_Taxonomy;
+
+class Taxonomy
+{
+    public $taxonomy;
+
+    public function __construct( $taxonomy)
+    {
+        $tax = get_taxonomy($taxonomy);
+        if($tax)
+        {
+            $this->taxonomy = $tax;
+        } 
+    }
+
+    public function isTaxonomy()
+    {
+        return $this->taxonomy instanceof WP_Taxonomy;
+    }
+
+    public function getName()
+    {
+        return $this->taxonomy->name;
+    }
+
+    public function getLabel()
+    {
+        return $this->taxonomy->label;
+    }
+}
