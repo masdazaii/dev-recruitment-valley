317b0ba59e0a983d46d2bb9fd3485c3e0b87582f
mimasdazai <anggit@madeindonesia.com>
2023-08-15 17:56:49 +0700

    add company into single vacancies

--- wp-content/themes/recruitment-valley/modules/API/model/Company.php
+++ wp-content/themes/recruitment-valley/modules/API/model/Company.php
@@ -8,6 +8,7 @@ use WP_Query;
 class Company
 {
     public $user_id;
+    public $user;
     public $vacancyModel;
 
     private $dateOfBirth = "ucma_date_of_birth";
@@ -15,10 +16,17 @@ class Company
     private $phoneCode = "ucma_phone_code";
     private $country = "ucma_country";
     private $city = "ucma_city";
-    private $linkedin = "ucma_linkedin_url_page";
     private $isFullRegistered = "ucma_is_full_registered";
-    private $cv = "ucma_cv";
     private $image = "ucma_image";
+    private $name = "ucma_company_name";
+    private $description = "ucma_short_decription";
+    private $totalEmployee = "ucma_employees";
+    private $website = "ucma_website_url";
+
+    private $facebook = "ucma_facebook_url";
+    private $twitter = "ucma_twitter_url";
+    private $instagram = "ucma_instagram_url";
+    private $linkedin = "ucma_linkedin_url";
 
     public function __construct($userId = false)
     {
@@ -26,6 +34,7 @@ class Company
         if($userId)
         {
             $this->user_id = $userId;
+            $this->user = get_user_by('id', $this->user_id);
         }
     }
 
@@ -57,8 +66,61 @@ class Company
 
     public function getThumbnail()
     {
-        $attachment_id = $this->getProp($this->image); 
+        $attachment_id = $this->getProp($this->image);
         return wp_get_attachment_url($attachment_id) ? wp_get_attachment_url($attachment_id) : null;
+    }
+
+    public function getName()
+    {
+        return $this->getProp($this->name);
+    }
+
+    public function getDescription()
+    {
+        return  $this->getProp($this->description);
+    }
+
+    public function getPhone()
+    {
+        return  $this->getProp($this->phone);
+    }
+
+    public function getPhoneCode()
+    {
+        return $this->getProp($this->phoneCode);
+    }
+
+    public function getEmail()
+    {
+        return $this->user->user_email;
+    }
+
+    public function getTotalEmployees()
+    {
+        return $this->getProp($this->totalEmployee);
+    }
+
+    public function getFacebook()
+    {
+        return $this->getProp($this->facebook);
+    }
+
+    public function getTwitter()
+    {
+        return $this->getProp($this->twitter);
+    }
+    public function getLinkedin()
+    {
+        return $this->getProp($this->linkedin);
+    }
+    public function getInstagram()
+    {
+        return $this->getProp($this->instagram);
+    }
+
+    public function getWebsite()
+    {
+        return $this->getProp($this->website);
     }
 
     public function getProp( $acf_field, $single = false)
