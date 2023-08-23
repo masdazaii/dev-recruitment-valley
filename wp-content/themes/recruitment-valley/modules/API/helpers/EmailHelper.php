5d8cb12d21a2909f3d9b88e0eacb4d44970cf9d2
mimasdazai <anggit@madeindonesia.com>
2023-08-23 09:15:50 +0700

    add payment confirmation email

--- wp-content/themes/recruitment-valley/modules/API/helpers/EmailHelper.php
+++ wp-content/themes/recruitment-valley/modules/API/helpers/EmailHelper.php
@@ -2,6 +2,12 @@
 
 namespace Helper;
 
+use BD\Emails\Email;
+use Exception;
+use Model\Company;
+use Package;
+use Transaction;
+
 class EmailHelper
 {
 
@@ -25,5 +31,37 @@ class EmailHelper
         $headers[] = 'From: ' . $sender['name'] . '<' . $sender['email']  . '>';
 
         wp_mail($receipient, $subject, $output, $headers);
+    }
+
+    public static function sendPaymentConfirmation( $transactionId )
+    {
+        try {
+            $transaction = new Transaction( $transactionId );
+
+            $companyId = $transaction->getUserId();
+
+            error_log($companyId);
+
+            $packageId = $transaction->getPackageId();
+
+            $company = new Company($companyId);
+            $package = new Package( $packageId );
+
+            $args = [
+                "company.contactPerson" => $company->getEmail(),
+                "company.name" => $company->getName(),
+            ];
+
+            $content = Email::render_html_email('payment-confirmation-credits-company.php', $args);
+            $site_title = get_bloginfo('name');
+
+            $headers = array(
+                'Content-Type: text/html; charset=UTF-8',
+            );
+
+            wp_mail($company->getEmail(), "payment confirmation - $site_title", $content, $headers);
+        } catch (Exception $e) {
+            error_log( $e->getMessage());
+        }
     }
 }
