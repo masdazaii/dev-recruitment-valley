625d61e45664afd574d6be383a96a6307cd7bdbf
Mirza Putra Meyhendra <mirza@madeindonesia.com>
2023-08-29 16:10:28 +0700

    fix font on email template

--- wp-content/themes/recruitment-valley/templates/email/contact-form-admin.php
+++ wp-content/themes/recruitment-valley/templates/email/contact-form-admin.php
@@ -319,7 +319,7 @@
                                       color: #000000;
                                     ">
                                   <p align="center" style="
-                                        font-family: Neue Montreal Regular;
+                                        font-family: Neue Montreal Regular, Tahoma;
                                         font-size: 24px;
                                         font-style: normal;
                                         font-weight: 500;
@@ -403,7 +403,7 @@
                                       color: #000000;
                                     ">
                                   <p class="text-title" align="center" style="
-                                        font-family: Neue Montreal Medium;
+                                        font-family: Neue Montreal Medium, Tahoma;
                                         font-size: 36px;
                                         font-style: normal;
                                         font-weight: 500;
@@ -411,7 +411,7 @@
                                         color: #234943;
                                       "></p>
                                   <p align="center" style="
-                                        font-family: Neue Montreal Regular;
+                                        font-family: Neue Montreal Regular, Tahoma;
                                         font-size: 16px;
                                         font-style: normal;
                                         font-weight: 400;
@@ -568,7 +568,7 @@
                                       color: #000000;
                                     ">
                                   <p class="text-title" style="
-                                        font-family: Neue Montreal Medium;
+                                        font-family: Neue Montreal Medium, Tahoma;
                                         font-size: 45px;
                                         font-style: normal;
                                         font-weight: 500;
@@ -631,14 +631,14 @@
                                     word-break: break-word;
                                   ">
                                 <div style="
-                                      font-family: Neue Montreal Light;
+                                      font-family: Neue Montreal Light, Tahoma;
                                       font-size: 13px;
                                       line-height: 1;
                                       text-align: left;
                                       color: #f45e43;
                                     ">
                                   <p style="
-                                        font-family: Neue Montreal Medium;
+                                        font-family: Neue Montreal Medium, Tahoma;
                                         \font-size: 14px;
                                         font-style: normal;
                                         font-weight: 500;
@@ -650,7 +650,7 @@
                                     Contact
                                   </p>
                                   <p style="
-                                        font-family: Neue Montreal Regular;
+                                        font-family: Neue Montreal Regular, Tahoma;
                                         font-size: 14px;
                                         font-style: normal;
                                         font-weight: 400;
@@ -692,14 +692,14 @@
                                     word-break: break-word;
                                   ">
                                 <div style="
-                                      font-family: Neue Montreal Light;
+                                      font-family: Neue Montreal Light, Tahoma;
                                       font-size: 13px;
                                       line-height: 1;
                                       text-align: left;
                                       color: #f45e43;
                                     ">
                                   <a href="mailto:info@recruitmentvalley.com" style="
-                                        font-family: Neue Montreal Medium;
+                                        font-family: Neue Montreal Medium, Tahoma;
                                         font-size: 14px;
                                         font-style: normal;
                                         font-weight: 500;
@@ -903,7 +903,7 @@
                                       color: #000000;
                                     ">
                                   <p align="center" style="
-                                        font-family: Neue Montreal Regular;
+                                        font-family: Neue Montreal Regular, Tahoma;
                                         font-size: 12px;
                                         font-style: normal;
                                         font-weight: 400;
