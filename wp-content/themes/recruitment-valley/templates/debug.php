41dba3cde3af09d0c00e359620203dd37169b124
zulfan <zulfan@madeindonesia.com>
2023-09-06 11:45:03 +0700

    restructure data  vacancies

--- wp-content/themes/recruitment-valley/templates/debug.php
+++ wp-content/themes/recruitment-valley/templates/debug.php
@@ -7,4 +7,12 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
 
 use JobAlert\Data;
 
+$data = new Data();
+$monthly = $data->main('monthly');
+
+echo '<pre>';
+var_dump($monthly);
+echo '</pre>';
+die;
+
 ?>
