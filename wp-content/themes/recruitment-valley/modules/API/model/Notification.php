0d286366a2295abba689536bea1963a907b95718
mimasdazai <anggit@madeindonesia.com>
2023-09-19 13:18:25 +0700

    finish notification helper

--- wp-content/themes/recruitment-valley/modules/API/model/Notification.php
+++ wp-content/themes/recruitment-valley/modules/API/model/Notification.php
@@ -4,9 +4,16 @@ namespace Model;
 
 use Exception;
 
-class Notifaction
+class Notification
 {
     private $wpdb;
+
+    private $table = "rv_notifications";
+
+    public $notification;
+    public $recipient_id;
+    public $recipient;
+    public $type;
 
     public function __construct( $notificationId = false )
     {
@@ -15,12 +22,60 @@ class Notifaction
 
         if($notificationId)
         {
-            $query = "select id from rv_notifications where id=". $notificationId;
-            $notification = $this->wpdb->get_results($query, "array");
+            $query = "select * from rv_notifications where id = ". $notificationId .";";
+            $notification = $this->wpdb->get_results($query);
             if(count($notification) > 0)
             {
+                $this->notification = $notification[0];
+            }else{
                 throw new Exception(__("notification not found", "recruitment_valley"), 404 );
             }
         }
+    }
+
+    public function getRecipient()
+    {
+        return $this->notification->recipient_id;
+    }
+
+    public function getData()
+    {
+        return $this->notification;
+    }
+
+    public function getType()
+    {
+        return $this->notification->notification_type;
+    }
+
+    public function getReadStatus()
+    {
+        return $this->notification->read_status == "0" ? false : true;
+    }
+
+    public function setNotification( $notificationId )
+    {
+        if($notificationId)
+        {
+            $query = "select * from rv_notifications where id = ". $notificationId .";";
+            $notification = $this->wpdb->get_results($query);
+            if(count($notification) > 0)
+            {
+                $this->notification = $notification[0];
+            }else{
+                throw new Exception(__("notification not found", "recruitment_valley"), 404 );
+            }
+        }
+    }
+    
+    /**
+     * set (update) notification entry, make sure the notification id already set 
+     *
+     * @param  mixed $payload
+     * @return void
+     */
+    public function set( $payload )
+    {
+        $this->wpdb->update($this->table, $payload, $this->notification->ID);
     }
 }
