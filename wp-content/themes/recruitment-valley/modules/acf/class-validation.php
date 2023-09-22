5dfaf2e456026fa577b1bb2b3cd626546860a323
Joshua Wicaksono <joshua@madindonesia.com>
2023-09-22 10:07:09 +0700

    Adjusts: get /company/vacancy/ response && validate favorite pacakge

--- wp-content/themes/recruitment-valley/modules/acf/class-validation.php
+++ wp-content/themes/recruitment-valley/modules/acf/class-validation.php
@@ -5,29 +5,30 @@ class MICustomAcfValidation
 {
     public function __construct()
     {
-        add_filter("acf/validate_value/name=is_favorite", [$this,"is_favorite_validation"], 10, 4);
+        add_filter("acf/validate_value/name=is_favorite", [$this, "is_favorite_validation"], 10, 4);
     }
 
-    public function is_favorite_validation( $valid, $value, $field, $input_name )
+    public function is_favorite_validation($valid, $value, $field, $input_name)
     {
-        $package = get_posts([
-            "post_type" => "package",
-            "post_status" => "publish",
-            "numberposts" => -1,
-            "meta_query" =>[
-                [
-                    "key" => "is_favorite",
-                    "value" => 1,
-                    "compare" => "="
-                ],
-            ]
-        ]);
+        if ($value == 1) {
+            $package = get_posts([
+                "post_type" => "package",
+                "post_status" => "publish",
+                "numberposts" => -1,
+                "meta_query" => [
+                    [
+                        "key" => "is_favorite",
+                        "value" => 1,
+                        "compare" => "="
+                    ],
+                ]
+            ]);
 
-        reset($package);
+            reset($package);
 
-        if(count($package) > 0 && (int) $_POST["post_ID"] != $package[0]->ID )
-        {
-            return __( 'Favorite product already set in ' . $package[0]->post_title, THEME_DOMAIN );
+            if (count($package) > 0 && (int) $_POST["post_ID"] != $package[0]->ID) {
+                return __('Favorite product already set in ' . $package[0]->post_title, THEME_DOMAIN);
+            }
         }
 
         return $valid;
