c204225447104673b1740b8f5f476adda936f313
mimasdazai <anggit@madeindonesia.com>
2023-10-09 11:48:00 +0700

    add basic model coupon

---
+++ wp-content/themes/recruitment-valley/modules/API/model/Coupon.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Model;
+
+use Exception;
+use WP_Query;
+
+class Coupon
+{
+    public $prefix = "coupon";
+
+    const STATUS_AVAILABLE_VALUE = "available";
+    const STATUS_AVAILABLE_LABEL = "Available";
+
+    const STATUS_RUN_OUT_VALUE = "run_out";
+    const STATUS_RUN_OUT_LABLE = "Run Out";
+
+    const STATUS_EXPIRED_VALUE = "expired";
+    const STATUS_EXPIRED_LABEL = "Expired";
+
+    const DISCOUNT_TYPE_FIX_AMOUNT_VALUE = "fix_amount";
+    const DISCOUNT_TYPE_FIX_AMOUNT_LABEL = "Fix Amount";
+
+    const DISCOUNT_TYPE_PERCENTAGE_VALUE = "percentage";
+    const DISCOUNT_TYPE_PERCENTAGE_LABEL = "Percentage";
+
+    public $couponID;
+    public $post;
+    public $porperties;
+
+    public $status = "status";
+    public $expiredAt = "expired_at";
+    public $code = "code";
+    public $discount_value = "discount_value";
+    public $discount_type = "discount_type";
+    public $description = "description";
+    
+    private $_couponDiscountValue;
+    private $_couponDiscountType;
+    private $_couponCode;
+
+
+
+    public function __construct($coupon_id = false)
+    {
+        if ($coupon_id) {
+            $this->couponID = $coupon_id;
+            $this->post = get_post($coupon_id);
+            $this->porperties = get_fields( $coupon_id );
+
+            if(!$this->post)
+            {
+                throw new Exception("Coupon not found", 400);
+            }
+        }
+    }
+
+    /**
+     * Setter function
+     *
+     * Set acf / meta / post data
+     *
+     * @return mixed
+     */
+    public function set($key, $value, $type = 'acf'): mixed
+    {
+        return  update_field($key, $value, $this->_couponCode);
+    }
+
+    /**
+     * Getter function
+     *
+     * @param string $key   : acf / meta key
+     * @param string $value : value to store
+     * @param string $type  : type of data to get, either meta or acf
+     * @return mixed : either object, array
+     */
+    public function get($key, $type = 'acf'): mixed
+    {
+        $key = $this->prefix. '_' . $key;
+        return $this->porperties[$key];
+    }
+
+    public function getExpiredAt() : int
+    {
+        $date = $this->get($this->expiredAt);
+        return strtotime($date);
+    }
+
+    public function getCode() : string
+    {
+        return $this->get($this->code);
+    }
+
+    public function getDiscountType() : array
+    {
+        return $this->get($this->discount_type);
+    }
+
+    public function getDescription() : string | null
+    {
+        return $this->get($this->description);
+    }
+
+    public function getStatus() : array
+    {
+        return $this->get($this->status);
+    }
+
+    public function getDiscountValue() : int
+    {
+        return $this->get($this->discount_value);
+    }
+}
