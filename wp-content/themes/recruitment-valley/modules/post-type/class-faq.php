6879ecc02cee858614068a23d1179a83c76278da
Joshua Wicaksono <joshua@madindonesia.com>
2023-10-26 10:24:18 +0700

    Add: List Faq Endpoint. Adjusts: Enable RSS.

---
+++ wp-content/themes/recruitment-valley/modules/post-type/class-faq.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PostType;
+
+defined('ABSPATH') or die('Direct access not allowed!');
+
+class FaqCPT extends RegisterCPT
+{
+    public function __construct()
+    {
+        add_action('init', [$this, 'RegisterFAQCPT']);
+    }
+
+    public function RegisterFAQCPT()
+    {
+        $title  = __('FAQ', THEME_DOMAIN);
+        $slug   = 'faq';
+        $args   = [
+            'menu_position' => 5,
+            'supports'      => array('title', 'editor', 'author', 'thumbnail'),
+        ];
+
+        $this->customPostType($title, $slug, $args);
+    }
+}
+
+// Initialize
+new FaqCPT();
