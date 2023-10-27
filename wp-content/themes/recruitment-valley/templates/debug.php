a0b23a787a0f7268d5a9aca9a9df2a8878327138
mimasdazai <anggit@madeindonesia.com>
2023-10-27 15:56:26 +0700

    progress import feed

--- wp-content/themes/recruitment-valley/templates/debug.php
+++ wp-content/themes/recruitment-valley/templates/debug.php
@@ -3,6 +3,7 @@
 use Integration\ActiveCampaign\ActiveCampaign;
 use Model\Coupon;
 use Model\Notification;
+use Aws\Exception\AwsException;
 
 defined( 'ABSPATH' ) || die( "Can't access directly" );
 
@@ -10,12 +11,64 @@ defined( 'ABSPATH' ) || die( "Can't access directly" );
  * Template Name: Debugging
  */
 
-$activeCampaign = new ActiveCampaign();
-$data = [
-   "firstName" => "anggit",
-   "lastName" => "prayoga", 
-   "email" => "Anggitp@gmail.com",
-   "phone" => "09409934128"
-];
+
+ require_once get_stylesheet_directory() . "/vendor/autoload.php";
+
+ try {
+
+   $dateNow = date('Y-m-d');
+
+   $s3 = new \Aws\S3\S3Client([
+      'region' => 'eu-central-1',
+      'version' => 'latest',
+      'credentials' => [
+         'key' => "AKIAWCFQSHUA36DCMDW7",
+         'secret' => "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",
+      ]
+    ]);
+   
+    $key = "NL/daily/".$dateNow."/jobs_new.0.jsonl.gz";
+    $fileName = wp_upload_dir()["basedir"]. '/aws/job/new/'.$dateNow.'.gz';
+    error_log(json_encode(wp_upload_dir()));
+
+    $result = $s3->getObject([
+      'Bucket' => 'jobfeed-intelligence-group',
+      'Key'    => $key,
+      'SaveAs' => $fileName,
+    ]);
+
+      // Raising this value may increase performance
+      $buffer_size = 4096000; // read 4kb at a time
+      $out_file_name = str_replace('.gz', '.jsonl', $fileName); 
+
+      // Open our files (in binary mode)
+      $file = gzopen($fileName, 'rb');
+      $out_file = fopen($out_file_name, 'wb'); 
+
+      // Keep repeating until the end of the input file
+      while (!gzeof($file)) {
+         // Read buffer-size bytes
+         // Both fwrite and gzread and binary-safe
+         fwrite($out_file, gzread($file, $buffer_size));
+      }
+
+      // Files are done, close files
+      fclose($out_file);
+      gzclose($file);
+      
+
+      $vacancies = file_get_contents( $out_file_name );
+      $vacancies = explode("\n", $vacancies);
+
+      foreach ($vacancies as $vacancy) {
+         echo '<pre>';
+         var_dump(json_decode($vacancy)->job_id);
+         echo '</pre>';
+      }
+ } catch (AwsException $e) {
+   echo '<pre>';
+   var_dump($e->getMessage());
+   echo '</pre>';
+ }
 
 ?>
