61bdf46bffa26c13651efb771ba7c8805a59e3c7
Joshua Wicaksono <joshua@madindonesia.com>
2023-10-30 17:10:49 +0700

    Fix: Send Reset Password link error.

--- wp-content/themes/recruitment-valley/modules/emails/autoload.php
+++ wp-content/themes/recruitment-valley/modules/emails/autoload.php
@@ -1,15 +1,17 @@
 <?php
+
 /**
  * Autoloading
  *
  * @package BornDigital
  */
 
-defined( 'ABSPATH' ) || die( "Can't access directly" );
+defined('ABSPATH') || die("Can't access directly");
 
 require_once __DIR__ . '/class-placeholder.php';
 require_once __DIR__ . '/class-setup.php';
 require_once __DIR__ . '/class-email.php';
+require_once __DIR__ . '/class-email-option.php'; // This is added line.
 
 // require classes.
 require_once __DIR__ . '/class-reset-password.php';
