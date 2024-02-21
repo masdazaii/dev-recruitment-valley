e0da7ff3e1277d5ba7e035fc88581a830a0250da
Joshua Wicaksono <joshua@madindonesia.com>
2024-02-21 18:34:26 +0700

    Onprogress: Byner integration.

--- wp-content/themes/recruitment-valley/modules/acf/class-custom-action.php
+++ wp-content/themes/recruitment-valley/modules/acf/class-custom-action.php
@@ -19,58 +19,60 @@ class AcfCustomAction
         try {
             $this->wpdb->query("START TRANSACTION");
             $post = get_post($post_id);
-            if ($post->post_type == "vacancy") {
-                $vacancy = new Vacancy($post_id);
+            if (property_exists($post, 'post_type')) {
+                if ($post->post_type == "vacancy") {
+                    $vacancy = new Vacancy($post_id);
 
-                $expired_at = get_field('expired_at', $post_id);
-                /** Changed Below */
-                // $expired_dates = maybe_unserialize(get_option("job_expires"));
+                    $expired_at = get_field('expired_at', $post_id);
+                    /** Changed Below */
+                    // $expired_dates = maybe_unserialize(get_option("job_expires"));
 
-                // $postFound = false;
-                // $new_expired_dates = array_map(function ($expired_date) use ($vacancy, $expired_at) {
-                //     if ($expired_date["post_id"] == $vacancy->vacancy_id) {
-                //         $expired_date["expired_at"] = $expired_at;
-                //         $postFound = true;
-                //     }
+                    // $postFound = false;
+                    // $new_expired_dates = array_map(function ($expired_date) use ($vacancy, $expired_at) {
+                    //     if ($expired_date["post_id"] == $vacancy->vacancy_id) {
+                    //         $expired_date["expired_at"] = $expired_at;
+                    //         $postFound = true;
+                    //     }
 
-                //     return $expired_date;
-                // }, $expired_dates);
+                    //     return $expired_date;
+                    // }, $expired_dates);
 
-                // if (!$postFound) {
-                //     array_push($new_expired_dates, ["post_id" => $post_id, "expired_at" => $expired_at]);
-                // }
+                    // if (!$postFound) {
+                    //     array_push($new_expired_dates, ["post_id" => $post_id, "expired_at" => $expired_at]);
+                    // }
 
-                /** Changes
-                 * Only store when expired_at is not empty
-                 * class/method that MAYBE INTERVENT WITH :
-                 * - class-vacancy.php -> setExpiredDate
-                 * - class-menu-import-approval -> after approve this will trigger the method : class-vacancy.php -> setExpiredDate
-                 */
-                if ($expired_at && !empty($expired_at)) {
-                    $expired_dates = maybe_unserialize(get_option("job_expires"));
+                    /** Changes
+                     * Only store when expired_at is not empty
+                     * class/method that MAYBE INTERVENT WITH :
+                     * - class-vacancy.php -> setExpiredDate
+                     * - class-menu-import-approval -> after approve this will trigger the method : class-vacancy.php -> setExpiredDate
+                     */
+                    if ($expired_at && !empty($expired_at)) {
+                        $expired_dates = maybe_unserialize(get_option("job_expires"));
 
-                    $postFound = false;
-                    $new_expired_dates = array_map(function ($expired_date) use ($vacancy, $expired_at) {
-                        if ($expired_date["post_id"] == $vacancy->vacancy_id) {
-                            $expired_date["expired_at"] = $expired_at;
-                            $postFound = true;
+                        $postFound = false;
+                        $new_expired_dates = array_map(function ($expired_date) use ($vacancy, $expired_at) {
+                            if ($expired_date["post_id"] == $vacancy->vacancy_id) {
+                                $expired_date["expired_at"] = $expired_at;
+                                $postFound = true;
+                            }
+
+                            return $expired_date;
+                        }, $expired_dates);
+
+                        if (!$postFound) {
+                            array_push($new_expired_dates, ["post_id" => $post_id, "expired_at" => $expired_at]);
                         }
 
-                        return $expired_date;
-                    }, $expired_dates);
-
-                    if (!$postFound) {
-                        array_push($new_expired_dates, ["post_id" => $post_id, "expired_at" => $expired_at]);
+                        error_log('post : ' . $post_id . ' - ' . $expired_at);
+                        error_log(json_encode($new_expired_dates));
+                        update_option("job_expires", $new_expired_dates);
                     }
 
-                    error_log('post : ' . $post_id . ' - ' . $expired_at);
-                    error_log(json_encode($new_expired_dates));
-                    update_option("job_expires", $new_expired_dates);
+
+                    $this->wpdb->query("COMMIT");
+                    error_log("options job expired updated");
                 }
-
-
-                $this->wpdb->query("COMMIT");
-                error_log("options job expired updated");
             }
         } catch (WP_Error $err) {
             $this->wpdb->query("ROLLBACK");
--- wp-content/themes/recruitment-valley/modules/acf/class-import-api-option.php
+++ wp-content/themes/recruitment-valley/modules/acf/class-import-api-option.php
@@ -9,6 +9,7 @@ class ImportApiSetting
     public function __construct()
     {
         $this->importApiOptionPage();
+        add_filter('pre_update_option_options_rv_byner_client_secret', [$this, 'encryptClientSecret'], 10, 3);
     }
 
     private function importApiOptionPage()
@@ -22,6 +23,30 @@ class ImportApiSetting
                 'redirect'      => false
             ]);
         }
+    }
+
+    public function encryptClientSecret(Mixed $value, Mixed $old, String $option)
+    {
+        if ($option == 'options_rv_byner_client_id') {
+            // $value =
+
+            /** Chyper Method */
+            $ciphering  = "BF-CBC";
+
+            /** Using OpenSSl encryption method */
+            $iv_length      = openssl_cipher_iv_length($ciphering);
+            $encryptOption  = 0;
+
+            /** Initialization vector */
+            $encryptionIV   = random_bytes($iv_length);
+
+            /** characters or numeric for iv */
+            $encryptionKey = openssl_digest(php_uname(), 'MD5', true);
+
+            /** Encryption of string process begins */
+            $encryption = openssl_encrypt($value, $ciphering, $encryptionKey, $encryptOption, $encryptionIV);
+        }
+        return $value;
     }
 }
 
